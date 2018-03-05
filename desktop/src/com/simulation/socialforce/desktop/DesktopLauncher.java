58ec604f4ec96eb219911067b3508be496e853cb
Ryusei Ishida <serys9469.japan@gmail.com>
2018-03-05 17:10:50 +0900

    :octocat: Initial commit

---
+++ desktop/src/com/simulation/socialforce/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.simulation.socialforce.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.simulation.socialforce.SocialForceModel;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SocialForceModel(), config);
+	}
+}
