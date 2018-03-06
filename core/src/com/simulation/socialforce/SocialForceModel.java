f66bde335c448deddba79e4e21b34f4194dcf830
Ryusei Ishida <serys9469.japan@gmail.com>
2018-03-06 16:10:08 +0900

    agent draw

--- core/src/com/simulation/socialforce/SocialForceModel.java
+++ core/src/com/simulation/socialforce/SocialForceModel.java
@@ -3,31 +3,52 @@ package com.simulation.socialforce;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+
 
 public class SocialForceModel extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+    private Texture personImage;
+	private SpriteBatch batch;
+	private OrthographicCamera camera;
+	private Rectangle person;
+
 	@Override
 	public void create () {
+
+		personImage = new Texture(Gdx.files.internal("person.png"));
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+        person = new Rectangle();
+        person.x = 400;
+        person.y = 240;
+        person.width = 64;
+        person.height = 64;
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(255, 255, 255, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		    batch.draw(personImage, person.x, person.y);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
+	    personImage.dispose();
 		batch.dispose();
-		img.dispose();
 	}
 }
