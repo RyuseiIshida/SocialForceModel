8085b0baf561b50766e9c4e028dba6d8f2cc18ae
Ryusei Ishida <serys9469.japan@gmail.com>
2018-03-06 19:43:39 +0900

    :heavy_plus_sign:: add wall Sprite

--- core/src/com/simulation/socialforce/SocialForceModel.java
+++ core/src/com/simulation/socialforce/SocialForceModel.java
@@ -5,24 +5,28 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
 
 
 public class SocialForceModel extends ApplicationAdapter {
     private Texture personImage;
-	private SpriteBatch batch;
-	private OrthographicCamera camera;
-	private Rectangle person;
+    private Texture wallImage;
+    private SpriteBatch batch;
+    private OrthographicCamera camera;
+    private Rectangle person;
+    private Array<Sprite> walls;
 
-	@Override
-	public void create () {
+    @Override
+    public void create () {
+        personImage = new Texture(Gdx.files.internal("person.png"));
+        wallImage = new Texture(Gdx.files.internal("wall.png"));
 
-		personImage = new Texture(Gdx.files.internal("person.png"));
-
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480);
-		batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+        batch = new SpriteBatch();
 
         person = new Rectangle();
         person.x = 800/2 - 32/2;
@@ -30,24 +34,52 @@ public class SocialForceModel extends ApplicationAdapter {
         person.width = 32;
         person.height = 32;
 
-	}
+        walls = new Array<Sprite>();
+        spawnWall();
+    }
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(255, 255, 255, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    private void spawnWall(){
+        for(int i = 10; i < 750; i += 32){
+            Sprite wall = new Sprite(wallImage);
+            wall.setPosition(i, 10);
+            walls.add(wall);
+            Sprite wall2 = new Sprite(wallImage);
+            wall2.setPosition(i, 450);
+            walls.add(wall2);
+        }
 
-		camera.update();
-		batch.setProjectionMatrix(camera.combined);
+        for(int i = 25; i < 450; i += 32){
+            Sprite wall = new Sprite(wallImage);
+            wall.setPosition(0, i);
+            wall.setRotation(90);
+            walls.add(wall);
+            Sprite wall2 = new Sprite(wallImage);
+            wall2.setPosition(760, i);
+            wall2.setRotation(90);
+            walls.add(wall2);
+        }
 
-		batch.begin();
-		    batch.draw(personImage, person.x, person.y);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-	    personImage.dispose();
-		batch.dispose();
-	}
+    }
+
+    @Override
+    public void render () {
+        Gdx.gl.glClearColor(255, 255, 255, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+
+        batch.begin();
+        batch.draw(personImage, person.x, person.y);
+        for(Sprite wall: walls){
+            wall.draw(batch);
+        }
+        batch.end();
+    }
+
+    @Override
+    public void dispose () {
+        personImage.dispose();
+        batch.dispose();
+    }
 }
