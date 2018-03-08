c06d2a9d3c1d976b0e6fd5cbaf70e7280cc09090
Ryusei Ishida <serys9469.japan@gmail.com>
2018-03-08 13:14:23 +0900

    :heavy_plus_sign:: クリックでエージェントを生成

--- core/src/com/simulation/socialforce/SocialForceModel.java
+++ core/src/com/simulation/socialforce/SocialForceModel.java
@@ -8,7 +8,10 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
+
+import java.util.Random;
 
 
 public class SocialForceModel extends ApplicationAdapter {
@@ -17,6 +20,7 @@ public class SocialForceModel extends ApplicationAdapter {
     private SpriteBatch batch;
     private OrthographicCamera camera;
     private Rectangle person;
+    private Array<Sprite> agents;
     private Array<Sprite> walls;
 
     @Override
@@ -34,8 +38,26 @@ public class SocialForceModel extends ApplicationAdapter {
         person.width = 32;
         person.height = 32;
 
+        agents = new Array<Sprite>();
+        spawnAgent();
         walls = new Array<Sprite>();
         spawnWall();
+    }
+
+    private void spawnAgent(){
+
+
+        /* ランダム生成
+        Random rand = new Random();
+        int randomNumber;
+        for(int i = 0; i < 10; i++) {
+            randomNumber = rand.nextInt(6) * 100;
+            System.out.println(randomNumber);
+            Sprite agent = new Sprite(personImage);
+            agent.setPosition(randomNumber, randomNumber);
+            agents.add(agent);
+        }
+        */
     }
 
     private void spawnWall(){
@@ -50,18 +72,20 @@ public class SocialForceModel extends ApplicationAdapter {
 
         for(int i = 25; i < 450; i += 32){
             if(i>215 && i<248){
-                System.out.println("kita");
             }
             else {
                 Sprite wall = new Sprite(wallImage);
                 wall.setPosition(132, i);
                 wall.setRotation(90);
                 walls.add(wall);
-                Sprite wall2 = new Sprite(wallImage);
-                wall2.setPosition(745, i);
-                wall2.setRotation(90);
-                walls.add(wall2);
             }
+        }
+
+        for(int i = 25; i < 450; i += 32){
+            Sprite wall2 = new Sprite(wallImage);
+            wall2.setPosition(745, i);
+            wall2.setRotation(90);
+            walls.add(wall2);
         }
 
     }
@@ -75,12 +99,21 @@ public class SocialForceModel extends ApplicationAdapter {
         batch.setProjectionMatrix(camera.combined);
 
         batch.begin();
-        batch.draw(personImage, person.x, person.y);
-        for(Sprite wall: walls){
-            wall.draw(batch);
-        }
+        for(Sprite agent: agents){ agent.draw(batch); }
+        for(Sprite wall: walls){ wall.draw(batch); }
         batch.end();
+
+        if(Gdx.input.isTouched()) { // クリックでエージェント生成
+            System.out.println("touched");
+            Vector3 touchPos = new Vector3();
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+            camera.unproject(touchPos);
+            Sprite agent = new Sprite(personImage);
+            agent.setPosition(touchPos.x-32/2, touchPos.y-32/2);
+            agents.add(agent);
+        }
     }
+
 
     @Override
     public void dispose () {
