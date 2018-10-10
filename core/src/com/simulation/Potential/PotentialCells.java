fdba597dab15e582cbe594970d54e38479f86e25
serys9469 <serys9469.japan@gmail.com>
2018-10-10 13:11:53 +0900

    refactor

--- core/src/com/simulation/Potential/PotentialCells.java
+++ core/src/com/simulation/Potential/PotentialCells.java
@@ -1,8 +1,7 @@
 package com.simulation.Potential;
 
-import com.simulation.Cell.Cell;
 import com.simulation.Cell.Cells;
 import com.simulation.socialforce.Parameter;
 
-public class PotentialCell extends Cell {
+public class PotentialCells extends Cells {
 }
